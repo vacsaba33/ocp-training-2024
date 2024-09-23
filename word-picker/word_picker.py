625b0f7187c32e3a9fc77f6d81e3d0e825cdf584
vit.attilacsaba@mkb.hu <vit.attilacsaba@mkb.hu>
2024-09-23 15:50:24 +0200

    word-picker v1

---
+++ word-picker/word_picker.py
@@ -0,0 +1,11 @@
+from random import choice
+from flask import Flask
+
+app = Flask(__name__)
+
+options = ["first", "second", "third"]
+
+@app.route("/")
+
+def pick_word():
+	return choice(options)
